b53565cbbb8c8710b983f7b7665daf069bca659e
mekabu <sleep.seep.sheep@gmail.com>
2021-09-07 23:21:14 +0900

    Joystickとしての初期設定

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include <Arduino.h>
+#include <Joystick.h>
+
+#define JOYSTICK_BUTTON_COUNT 17
+
+Joystick_ Joystick = Joystick_(
+  JOYSTICK_DEFAULT_REPORT_ID,         // hidReportId 
+  JOYSTICK_TYPE_GAMEPAD,              // joystickType 
+  JOYSTICK_BUTTON_COUNT,              // buttonCount 
+  JOYSTICK_DEFAULT_HATSWITCH_COUNT,   // hatSwitchCount 
+  true,                               // includeXAxis
+  true,                               // includeYAxis 
+  true,                               // includeZAxis 
+  false,                              // includeRxAxis
+  false,                              // includeRyAxis
+  true,                               // includeRzAxis
+  false,                              // includeRudder
+  false,                              // includeThrottle
+  false,                              // includeAccelerator
+  false,                              // includeBrake
+  false                               // includeSteering
+);
 
 void setup() {
+  Joystick.begin();
 }
 
 void loop() {

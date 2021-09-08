2de825c90af61574fd1f6a7590834a2767c4b1d5
mekabu <sleep.seep.sheep@gmail.com>
2021-09-08 20:43:41 +0900

    ボタン入力処理と方向入力の追加

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,22 @@
 #include <Bounce2.h>
 
 // button assign const
-const uint8_t joystickButtonCount = 13;
-const uint8_t buttonPin[joystickButtonCount] = {12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-const uint8_t arrowPin[4] = {20, 21, 22, 23};
+const uint8_t buttonCount = 13;
+const uint8_t directionCount = 4;
+const uint8_t buttonPin[buttonCount] = {12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+const uint8_t directionPin[directionCount] = {20, 21, 22, 23};
 const uint8_t lsPin = 14;
 const uint8_t rsPin = 15;
 
 // debounce const
-Bounce debouncer[joystickButtonCount];
+Bounce buttonDebouncer[buttonCount];
+Bounce directionDebouncer[directionCount];
 const uint8_t debounceTime = 2;
 
 Joystick_ Joystick = Joystick_(
   JOYSTICK_DEFAULT_REPORT_ID,       // hidReportId 
   JOYSTICK_TYPE_GAMEPAD,            // joystickType 
-  joystickButtonCount,              // buttonCount 
+  buttonCount,              // buttonCount 
   1,                                // hatSwitchCount 
   true,                             // includeXAxis
   true,                             // includeYAxis 
@@ -32,12 +34,22 @@ Joystick_ Joystick = Joystick_(
 );
 
 void setup() {
-  for (uint8_t i = 0; i < joystickButtonCount; i++) {
-    debouncer[i] = Bounce();
-    debouncer[i].attach(buttonPin[i], INPUT_PULLUP);
-    debouncer[i].interval(debounceTime);
-    debouncer[i].update();
+  for (uint8_t i = 0; i < buttonCount; i++) {
+    buttonDebouncer[i] = Bounce();
+    buttonDebouncer[i].attach(buttonPin[i], INPUT_PULLUP);
+    buttonDebouncer[i].interval(debounceTime);
+    buttonDebouncer[i].update();
   }
+
+  for (uint8_t i = 0; i < directionCount; i++) {
+    directionDebouncer[i] = Bounce();
+    directionDebouncer[i].attach(directionPin[i], INPUT_PULLUP);
+    directionDebouncer[i].interval(debounceTime);
+    directionDebouncer[i].update();
+  }
+
+  pinMode(lsPin, INPUT_PULLUP);
+  pinMode(rsPin, INPUT_PULLUP);
 
   Joystick.begin();
   Joystick.setXAxisRange(0, 2);
@@ -52,10 +64,14 @@ void setup() {
 }
 
 void loop() {
-  while(1){
-    for (uint8_t i = 0; i < joystickButtonCount; i++) {
-      debouncer[i].update();
-      Joystick.setButton(i, !debouncer[i].read());
+  while(1) {
+    for (uint8_t i = 0; i < buttonCount; i++) {
+      buttonDebouncer[i].update();
+      Joystick.setButton(i, !buttonDebouncer[i].read());
     }
+    for (uint8_t i = 0; i < directionCount; i++) {
+      directionDebouncer[i].update();
+    }
+    // ToDo:方向入力の判定
   }
 }

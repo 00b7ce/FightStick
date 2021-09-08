868283f504284bafb184e852c5cb2674de59bac9
mekabu <sleep.seep.sheep@gmail.com>
2021-09-08 19:31:54 +0900

    入力系のピンアサインを追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,28 @@
 #include <Arduino.h>
 #include <Joystick.h>
 
-#define JOYSTICK_BUTTON_COUNT 13
+const uint8_t joystickButtonCount = 13;
+const uint8_t buttonPin[joystickButtonCount] = {12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+const uint8_t arrowPin[4] = {20, 21, 22, 23};
+const uint8_t lsPin = 14;
+const uint8_t rsPin = 15;
 
 Joystick_ Joystick = Joystick_(
-  JOYSTICK_DEFAULT_REPORT_ID,         // hidReportId 
-  JOYSTICK_TYPE_GAMEPAD,              // joystickType 
-  JOYSTICK_BUTTON_COUNT,              // buttonCount 
-  1,                                  // hatSwitchCount 
-  true,                               // includeXAxis
-  true,                               // includeYAxis 
-  true,                               // includeZAxis 
-  false,                              // includeRxAxis
-  false,                              // includeRyAxis
-  true,                               // includeRzAxis
-  false,                              // includeRudder
-  false,                              // includeThrottle
-  false,                              // includeAccelerator
-  false,                              // includeBrake
-  false                               // includeSteering
+  JOYSTICK_DEFAULT_REPORT_ID,       // hidReportId 
+  JOYSTICK_TYPE_GAMEPAD,            // joystickType 
+  joystickButtonCount,              // buttonCount 
+  1,                                // hatSwitchCount 
+  true,                             // includeXAxis
+  true,                             // includeYAxis 
+  true,                             // includeZAxis 
+  false,                            // includeRxAxis
+  false,                            // includeRyAxis
+  true,                             // includeRzAxis
+  false,                            // includeRudder
+  false,                            // includeThrottle
+  false,                            // includeAccelerator
+  false,                            // includeBrake
+  false                             // includeSteering
 );
 
 void setup() {
@@ -31,6 +35,7 @@ void setup() {
   Joystick.setYAxis(1);
   Joystick.setZAxis(1);
   Joystick.setRzAxis(1);
+  Joystick.setHatSwitch(0, -1);
 }
 
 void loop() {
